31eec38e5c4721488512d5d694d715f49e0d0aa7
Kaushan-Dutta <kaushandutta5@gmail.com>
2024-07-11 14:53:26 +0530

    initial commit

--- src/app/(auth)/register/[[...register]]/components/agencyFormPage/Location.tsx
+++ src/app/(auth)/register/[[...register]]/components/agencyFormPage/Location.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import { Button, TextField } from "@mui/material";
+import React,{useState} from 'react'
+import { Button, TextField ,Stack} from "@mui/material";
+import CachedIcon from '@mui/icons-material/Cached';
+import {useGeolocation, useStartTyping} from 'react-use';
 
 const Location:React.FC = () => {
+  const [location,setLocation] = useState<string>("Set Location"); 
+
   return (
     <div className="flex flex-col gap-5">
         <TextField
@@ -16,7 +20,23 @@ const Location:React.FC = () => {
           size="medium"
           sx={{ width: "100%" }}
         />
-        
+        <Stack direction="row" gap="10px">
+            <Button variant="contained" color="error" sx={{width:"200px",h:"20px"}} onClick={()=>{
+              if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                  console.log("longitude",position)
+                });
+              } else {
+                console.log("Geolocation is not available in your browser.");
+              }
+            }}>
+                {location}
+            </Button>
+            <Button variant="text" color="info" sx={{width:"20px",h:"20px"}} >
+                <CachedIcon/>
+            </Button>
+        </Stack>
+
       </div>
   )
 }
